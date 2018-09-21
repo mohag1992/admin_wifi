8019dfd8b6c40d13a2419ed842a1204aeeb48a48
mohmohaung@aceplussolutions.com <mohmohaung@aceplussolutions.com>
2018-09-22 01:26:58 +0630

    Add New Repository

---
+++ app/Http/Controllers/FirebaseController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Kreait\Firebase;
+
+use Kreait\Firebase\Factory;
+
+use Kreait\Firebase\ServiceAccount;
+
+use Kreait\Firebase\Database;
+
+class FirebaseController extends Controller
+
+{
+
+//
+
+public function index(){
+
+$serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/laravelfirebase-51535-firebase-adminsdk-cjsp6-8002436595.json');
+
+$firebase = (new Factory)
+
+->withServiceAccount($serviceAccount)
+
+->withDatabaseUri('https://laravelfirebase-51535.firebaseio.com/')
+
+->create();
+
+$database = $firebase->getDatabase();
+
+$newPost = $database
+
+->getReference('blog/posts')
+
+->push([
+
+'title' => 'Post title',
+
+'body' => 'This should probably be longer.'
+
+]);
+
+//$newPost->getKey(); // => -KVr5eu8gcTv7_AHb-3-
+
+//$newPost->getUri(); // => https://my-project.firebaseio.com/blog/posts/-KVr5eu8gcTv7_AHb-3-
+
+//$newPost->getChild('title')->set('Changed post title');
+
+//$newPost->getValue(); // Fetches the data from the realtime database
+
+//$newPost->remove();
+
+echo"<pre>";
+
+print_r($newPost->getvalue());
+
+}
+
+}
+
+?>
